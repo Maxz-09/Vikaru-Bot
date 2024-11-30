ab907965083b2a3e1004c0cf15e8fbbd81744333
Maxtream_09 <110289636+Maxz-09@users.noreply.github.com>
2024-11-30 15:48:40 +0700

    update vikaru.sh

--- vikaru.sh
+++ vikaru.sh
@@ -1,196 +1,152 @@
 #!/bin/bash
 
-# Author - github : Maxz-09
-# Author - no wa : +6289508899033
+# DELETING THE WATERMARK IS FORBIDDEN, RESPECT THE DEVELOPER
 
-# Path to the file that stores the password
-PASSWORD_FILE=".pw.txt"
+# -  Script by   : Maxtream_09
+# -  Github.     : https://github.com/Maxz-09
+# -  Sosmed    : https://linktr.ee/Maxtream_09
 
-# Color List
+
+# Color definitions (optional, comment out if not needed)
 white="\033[00m"
 red="\033[31m"
 green="\033[32m"
 yellow="\033[33m"
 
+
 # Confirm resource check 
 enter() {
     clear
-    echo -e -n $white;figlet "  Check file"
-    echo -e $white "-----------------------------"
-    echo -e "  Author : Maxtream_09"
-    echo -e "  GitHub : Maxz-09"
-    sleep 1
     echo
-    echo -e $white " # [!] Checking system files, make sure your internet is Good!"|pv -qL 30
-    sleep 1
+    echo -e $white " # [!] Checking system file. make sure your internet is Good!"
     read -r -s -p $'  • [?] Press enter to continue....\n'
     function back_option () {
         clear 
     }
-    cekfile
-    }
-
-# Cek Resources
-cekfile() {
+    
+# Install required resources
     echo
-    echo -e $white "=-----------------------------"
-    echo -e $yellow " # [!] Detect resource...."
-    echo -e $white "=-----------------------------"
+    echo -e $white "=------------------------------="
+    echo -e $yellow " # [!] Detect resource..."
+    echo -e $white "=------------------------------="
+    sleep 1
+    check_resource "figlet"
+    check_resource "pv"
+    check_resource "git"
+    check_resource "bash"
+    check_resource "nodejs-lts"
+    check_resource "libwebp"
+    check_resource "ffmpeg"
+    echo -e $white "=------------------------------="
+    sleep 1
+    echo -e $green " # [✓] All resources installed!"|pv -qL 35
     sleep 2
-    dpkg -s figlet &> /dev/null
-if [ $? -eq 0 ];then
-    echo -e $green " • [✓] Figlet installed"
-    else
-    echo
-    sleep 1
-    echo -e $yellow " • [/] Installing Figlet...."|pv -qL 30
-    echo -e $white " "
-    apt install figlet -y
-    echo
-    sleep 1
-    echo -e $green " • [✓] Figlet installed"
-    echo
-fi
-    dpkg -s pv &> /dev/null
-if [ $? -eq 0 ];then
-    echo -e $green " • [✓] Pv installed"
-    else
-    echo
-    sleep 1
-    echo -e $yellow " • [/] Installing Pv...."|pv -qL 30
-    echo -e $white " "
-    apt install pv -y
-    echo
-    sleep 1
-    echo -e $green " • [✓] Pv installed"
-    echo
-fi
-    dpkg -s git &> /dev/null
-if [ $? -eq 0 ];then
-    echo -e $green " • [✓] Git installed"
-    else
-    echo
-    sleep 1
-    echo -e $yellow " • [/] Installing Git...."|pv -qL 30
-    echo -e $white " "
-    apt install git -y
-    echo
-    sleep 1
-    echo -e $green " • [✓] Git installed"
-    echo
-fi
-    dpkg -s bash &> /dev/null
-if [ $? -eq 0 ];then
-    echo -e $green " • [✓] Bash installed"
-    else
-    echo
-    sleep 1
-    echo -e $yellow " • [/] Installing Bash...."|pv -qL 30
-    echo -e $white " "
-    apt install bash -y
-    echo
-    sleep 1
-    echo -e $green " • [✓] Bash installed"
-    echo
-fi
-    dpkg -s nodejs-lts &> /dev/null
-if [ $? -eq 0 ];then
-    echo -e $green " • [✓] Nodejs-lts installed"
-    else
-    echo
-    sleep 1
-    echo -e $yellow " • [/] Installing Nodejs-lts...."|pv -qL 30
-    echo -e $white " "
-    apt install nodejs-lts -y
-    echo
-    sleep 1
-    echo -e $green " • [✓] Nodejs-lts installed"
-    echo
-fi
-    dpkg -s libwebp &> /dev/null
-if [ $? -eq 0 ];then
-    echo -e $green " • [✓] Libwebp installed"
-    else
-    echo
-    sleep 1
-    echo -e $yellow " • [/] Installing Libwebp...."|pv -qL 30
-    echo -e $white " "
-    apt install libwebp -y
-    echo
-    sleep 1
-    echo -e $green " • [✓] Libwebp installed"
-    echo
-fi
-    dpkg -s ffmpeg &> /dev/null
-if [ $? -eq 0 ];then
-    echo -e $green " • [✓] Ffmpeg installed"
-    else
-    echo
-    sleep 1
-    echo -e $yellow " • [/] Installing Ffmpeg...."|pv -qL 30
-    echo -e $white " "
-    apt install ffmpeg -y
-    echo
-    sleep 1
-    echo -e $green " • [✓] Ffmpeg installed"
-    echo
-    fi
-    echo -e $white "-----------------------------"
-    sleep 2
-    echo -e $green " # [✓] Succssesfully"|pv -qL 30
-    sleep 2
-    clear
     menu
 }
 
-# Vikaru-Menu
-menu() {
-    echo -e -n $white;figlet "Vikaru-Menu"
-    echo -e $white "=-----------------------------"
-    echo -e "  # Select an options [1-6] :"
-    echo -e $white "=-----------------------------"
-    echo -e " [1] Create bot directory ( 1x only )"
-    echo -e " [2] Ar-Vikaru-Bot ( install )"
-    echo -e " [3] Base-Vikaru-Md ( install )"
-    echo -e " [4] Base-Vikaru-Md ( start )"
-    echo -e " [5] All script ( update )"
-    echo -e " [6] Exit"
+
+
+# Function to check resource installation
+check_resource() {
+  package_name="$1"
+  dpkg -s $package_name &> /dev/null
+  if [ $? -eq 0 ]; then
+    echo -e $green " • [✓] $package_name installed"
+    else
+    echo -e $yellow " • [/] Installing $package_name..."
     echo
-    mainmenu
+    pkg install "$package_name" -y
+    echo
+    echo -e $green " • [✓] $package_name installed"
+    echo
+  fi
+}
+
+# Main menu function (can be further simplified)
+menu() {
+    clear
+    echo -e -n $white;figlet "Vikaru-Menu"
+    echo -e $white "=------------------------------="
+    echo -e "  # Select an options [ 1-3 ] :"
+    echo -e $white "=------------------------------="
+    echo -e " [1] Install Script"
+    echo -e " [2] Update Script"
+    echo -e " [3] Create bot directory ( one-time )"
+    echo -e " [0] Exit"
+    echo
+    read -p " # Select > " choice
+    
+    case "$choice" in
+    1)
+      # Install All script
+      install
+      ;;
+      2)
+      # Update All script
+      update
+      ;;
+    3)
+      # Create bot directory
+      clear
+      echo -e -n $white;figlet "  Mkdir"
+      echo -e $white "=----------------------------------="
+      echo -e "  Author : Maxtream_09"
+      echo -e "  GitHub : Maxz-09"
+      sleep 1
+      echo
+      mkdir /storage/emulated/0/.vikaru-bot
+      mv vikaru.sh /storage/emulated/0/.vikaru-bot
+      mv .git /storage/emulated/0/.vikaru-bot
+      echo "echo 'Start : cd /storage/emulated/0/.vikaru-bot && bash vikaru.sh'" > "vikaru.sh"
+      echo -e $white "=----------------------------------="
+      echo -e $green " # [✓] mkdir /storage/emulated/0/.vikaru-bot"|pv -qL 30
+      sleep 1
+      echo -e $green " # [✓] mv vikaru.sh"|pv -qL 30
+      echo -e $white "=----------------------------------="
+      sleep 2
+      echo -e $yellow " # [!] Start this cmd :"
+      echo -e $white " "
+      echo -e $white " cd /storage/emulated/0/.vikaru-bot && bash vikaru.sh"|pv -qL 30
+      exit
+      ;;
+    0)
+      exit 0
+      ;;
+    *)
+      echo
+      echo -e $red" # [!] Input denied"|pv -qL 30
+      sleep 1
+      menu
+      ;;
+  esac
+}
+
+
+# Vikaru-Install
+install() {
+    clear
+    echo -e -n $white;figlet "Vikaru-Install"
+    echo -e $white "=------------------------------="
+    echo -e "  # Select an options [ 1-3 ] :"
+    echo -e $white "=------------------------------="
+    echo -e " [1] Ar-Vikaru-Bot"
+    echo -e " [2] Base-Vikaru-Md"
+    echo -e " [3] Base-Vikaru-Md_demo"
+    echo -e " [0] Back"
+    echo
+    maininstall
     }
 
-# Main menu
-mainmenu() {
+# Main install   
+maininstall() {
     echo -e -n " # Select > ";read pil
     echo
 if [ $pil == "1" ];then
-    clear
-    echo -e -n $white;figlet "  Mkdir"
-    echo -e $white "-----------------------------"
-    echo -e "  Author : Maxtream_09"
-    echo -e "  GitHub : Maxz-09"
-    sleep 1
-    echo
-    mkdir /storage/emulated/0/.vikaru-bot
-    mv vikaru.sh /storage/emulated/0/.vikaru-bot
-    mv .git /storage/emulated/0/.vikaru-bot
-    mv .pw.txt /storage/emulated/0/.vikaru-bot
-    echo "echo 'cd /storage/emulated/0/.vikaru-bot && bash vikaru.sh'" > "vikaru.sh"
-    echo -e $white "-----------------------------"
-    echo -e $green " # [✓] mkdir /storage/emulated/0/.vikaru-bot"|pv -qL 30
-    sleep 1
-    echo -e $green " # [✓] mv vikaru.sh"|pv -qL 30
-    echo -e $white "-----------------------------"
-    sleep 2
-    echo -e $yellow " # [!] Start this cmd :"
-    echo -e $white " "
-    echo -e $white " cd /storage/emulated/0/.vikaru-bot && bash vikaru.sh"|pv -qL 30
-    exit
-elif [ $pil == "2" ];then
+    # Install Ar-Vikaru-Bot
     clear
     cd /storage/emulated/0/.vikaru-bot
     echo -e -n $white;figlet "  Vikaru-Bot"
-    echo -e $white "-----------------------------"
+    echo -e $white "=----------------------------------="
     echo -e "  Author : Maxtream_09"
     echo -e "  GitHub : Maxz-09"
     sleep 1
@@ -202,12 +158,13 @@ elif [ $pil == "2" ];then
     echo 
     echo -e $green " # [✓] Succssesfully"|pv -qL 30
     sleep 2
-    menu
-elif [ $pil == "3" ];then
+    install
+elif [ $pil == "2" ];then
+    # Install Base-Vikaru-Md
     clear
     cd /storage/emulated/0/.vikaru-bot
     echo -e -n $white;figlet "  Vikaru-Md"
-    echo -e $white "-----------------------------"
+    echo -e $white "=----------------------------------="
     echo -e "  Author : Maxtream_09"
     echo -e "  GitHub : Maxz-09"
     sleep 1
@@ -217,123 +174,126 @@ elif [ $pil == "3" ];then
     git clone https://github.com/Maxz-09/base-vikaru-md
     sleep 1
     echo 
-    echo -e $yellow " • [/] Installing module...."|pv -qL 30
-    echo -e $white " "
-    cd /storage/emulated/0/.vikaru-bot/base-vikaru-md
-    yarn install -y
-    echo 
     echo -e $green " # [✓] Succssesfully"|pv -qL 30
     sleep 2
-    menu
-elif [ $pil == "4" ];then
+    install
+elif [ $pil == "3" ];then
+    # Install Base-Vikaru-Md_demo
     clear
     cd /storage/emulated/0/.vikaru-bot
-    echo -e -n $white;figlet "  Start-Md"
-    echo -e $white "-----------------------------"
+    echo -e -n $white;figlet "  Vikaru-Md_demo"
+    echo -e $white "=----------------------------------="
     echo -e "  Author : Maxtream_09"
     echo -e "  GitHub : Maxz-09"
     sleep 1
-    echo
-    echo -e $green " • [/] Start bot...."|pv -qL 30
+    echo 
+    echo -e $yellow " • [/] Installing Bot...."|pv -qL 30
+    echo -e $white " " 
+    git clone https://github.com/Maxz-09/base-vikaru-md_demo
     sleep 1
-    echo -e $white
-    cd base-vikaru-md
-    npm start
-elif [ $pil == "5" ];then
+    echo 
+    echo -e $green " # [✓] Succssesfully"|pv -qL 30
+    sleep 2
+    install
+elif [ $pil == "0" ];then
+    clear
+    echo -e $green" # [✓] Back"
+    menu
+else
+    echo -e $red" # [!] Input denied"|pv -qL 30
+    sleep 1
+    install
+fi
+}
+
+
+
+# Vikaru-Update
+update() {
+    clear
+    echo -e -n $white;figlet "Vikaru-Update"
+    echo -e $white "=------------------------------="
+    echo -e "  # Select an options [ 1-4 ] :"
+    echo -e $white "=------------------------------="
+    echo -e " [1] Vikaru.sh"
+    echo -e " [2] Ar-Vikaru-Bot"
+    echo -e " [3] Base-Vikaru-Md"
+    echo -e " [4] Base-Vikaru-Md_demo"
+    echo -e " [0] Back"
     echo
+    mainupdate
+}
+
+# Main update   
+mainupdate() {
+    echo -e -n " # Select > ";read pil
+    echo
+if [ $pil == "1" ];then
+    # Update vikaru.sh
     echo -e $yellow " # [/] Update..."|pv -qL 30
-    echo -e $white "-----------------------------"
+    echo -e $white "=----------------------------------="
     echo -e $green " • [/] Vikaru-Menu :"
     echo -e $white " "
     cd /storage/emulated/0/.vikaru-bot/
     git config --global --add safe.directory /storage/emulated/0/.vikaru-bot/
     git pull
-    echo
+    echo -e $white "=----------------------------------="
+    sleep 1
+    echo -e $green " # [✓]  Succssesfully"|pv -qL 30
+    sleep 2
+    update
+elif [ $pil == "2" ];then
+    # Update Ar-Vikaru-Bot
+    echo -e $yellow " # [/] Update..."|pv -qL 30
+    echo -e $white "=----------------------------------="
     echo -e $green " • [/] Ar-Vikaru-Bot :"
     echo -e $white
     cd /storage/emulated/0/.vikaru-bot/ar-vikaru-bot
     git config --global --add safe.directory /storage/emulated/0/.vikaru-bot/ar-vikaru-bot/
     git pull
-    echo -e $white "-----------------------------"
+    echo -e $white "=----------------------------------="
     sleep 1
     echo -e $green " # [✓]  Succssesfully"|pv -qL 30
-    sleep 3
-    exit
-elif [ $pil == "6" ];then
-    echo -e $green" # [✓] Exit"
-    exit 1
+    sleep 2
+    update
+elif [ $pil == "3" ];then
+    # Update Base-Vikaru-Md
+    echo -e $yellow " # [/] Update..."|pv -qL 30
+    echo -e $white "=----------------------------------="
+    echo -e $green " • [/] Ar-Vikaru-Md :"
+    echo -e $white
+    cd /storage/emulated/0/.vikaru-bot/base-vikaru-md
+    git config --global --add safe.directory /storage/emulated/0/.vikaru-bot/base-vikaru-md/
+    git pull
+    echo -e $white "=----------------------------------="
+    sleep 1
+    echo -e $green " # [✓]  Succssesfully"|pv -qL 30
+    sleep 2
+    update
+elif [ $pil == "4" ];then
+    # Update Base-Vikaru-Md_demo
+    echo -e $yellow " # [/] Update..."|pv -qL 30
+    echo -e $white "=----------------------------------="
+    echo -e $green " • [/] Ar-Vikaru-Md_demo :"
+    echo -e $white
+    cd /storage/emulated/0/.vikaru-bot/base-vikaru-md_demo
+    git config --global --add safe.directory /storage/emulated/0/.vikaru-bot/base-vikaru-md_demo/
+    git pull
+    echo -e $white "=----------------------------------="
+    sleep 1
+    echo -e $green " # [✓]  Succssesfully"|pv -qL 30
+    sleep 2
+    update
+elif [ $pil == "0" ];then
+    clear
+    echo -e $green" # [✓] Back"
+    menu
 else
     echo -e $red" # [!] Input denied"|pv -qL 30
     sleep 1
-    menu
+    update
 fi
 }
 
-# Fungsi untuk meminta password baru
-set_password() {
-    clear
-    echo -e -n $white;figlet "  Sign up"
-    echo -e $white "-----------------------------"
-    echo -e "  Author : Maxtream_09"
-    echo -e "  GitHub : Maxz-09"
-    echo
-    echo -e $white "=-----------------------------"
-    echo -e $yellow " # [!] Please create your password...."
-    echo -e $white "=-----------------------------"
-    sleep 1
-    read -p $'  • [?] Enter Password   : ' new_password
-    read -s -p $'  • [?] Confirm password : ' confirm_password
-    echo
-    echo -e $white "-----------------------------"
-    sleep 1
-
-    if [ "$new_password" != "$confirm_password" ]; then
-        echo -e $red " # [x] password not found, try again..."|pv -qL 30
-        sleep 2
-        set_password
-    else
-        echo "$new_password" > "$PASSWORD_FILE"
-        echo -e $green " # [✓] Password successfully set."|pv -qL 30
-        sleep 1
-        echo
-        echo -e $yellow " # [!] Start this cmd :" $white "bash vikaru.sh"|pv -qL 30
-        echo -e $white " "
-    fi
-}
-
-# Fungsi untuk memeriksa password
-check_password() {
-    clear
-    echo -e -n $white;figlet "  Login"
-    echo -e $white "-----------------------------"
-    echo -e "  Author : Maxtream_09"
-    echo -e "  GitHub : Maxz-09"
-    echo
-    sleep 1
-    echo -e $white " # [!] Please login to continue...."|pv -qL 30
-    sleep 1
-    read -s -p $'  • [?] Enter password : ' entered_password
-    echo
-    echo -e $white "-----------------------------"
-    sleep 1
-
-    stored_password=$(<"$PASSWORD_FILE")
-    
-    if [ "$entered_password" == "$stored_password" ]; then
-        echo -e $green " # [✓] Login successfully"|pv -qL 30
-        sleep 2
-        enter
-    else
-        echo -e $red " # [x] Password not found, try again....."|pv -qL 30
-        sleep 2
-        echo -e $white " "
-        check_password
-    fi
-}
-
-# Periksa apakah password sudah diatur
-if [ ! -f "$PASSWORD_FILE" ]; then
-    set_password
-else
-    check_password
-fi
+# Call the main menu function
+enter
